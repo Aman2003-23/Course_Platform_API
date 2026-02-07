dad5a3e680789145e34b09c85457e93d8a568d2c
Aman <amanagarwal0723@gmail.com>
2026-02-07 23:04:22 +0530

    updated securityConfig

--- src/main/java/com/example/Assignment/security/config/SecurityConfig.java
+++ src/main/java/com/example/Assignment/security/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/auth/**",
 
                                 //swagger ui
